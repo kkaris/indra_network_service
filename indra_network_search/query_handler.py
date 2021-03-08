c21100c7509755e5e87cc61af539212fbf953ce0
kkaris <karis.klas@gmail.com>
2021-03-08 09:43:18 -0500

    Update query mapping logic, typing, imports, __all__

--- indra_network_search/query_handler.py
+++ indra_network_search/query_handler.py
@@ -2,19 +2,18 @@
 The QueryHandler's job is to act as a middle layer between the network
 search functionalities and the REST API that receives queries.
 """
-import networkx as nx
-from typing import Optional, Dict
+from typing import Optional, List, Tuple, Union
 
 from indra.explanation.pathfinding import shortest_simple_paths, bfs_search,\
     open_dijkstra_search
 from depmap_analysis.network_functions.net_functions import SIGN_TO_STANDARD
 
 from .query import Query, ShortestSimplePathsQuery, BreadthFirstSearchQuery, \
-    DijkstraQuery
+    DijkstraQuery, SharedTargetsQuery, OntologyQuery, SharedRegulatorsQuery
 from .data_models import NetworkSearchQuery
+from .pathfinding import shared_parents
 
-__all__ = ['QueryHandler', 'Query', 'DijkstraQuery',
-           'ShortestSimplePathsQuery', 'BreadthFirstSearchQuery']
+__all__ = ['QueryHandler']
 
 
 class QueryHandler:
@@ -32,59 +31,50 @@ class QueryHandler:
         self.weighted: bool = bool(rest_query.weighted)
         self.mesh: bool = bool(rest_query.mesh_ids)
         self.strict_mesh: bool = rest_query.strict_mesh_id_filtering
-        self.api_method: Optional[str] = None
-        self._query: Optional[Query] = None
+        self._query_list: Optional[List[Tuple[str, Query]]] = None
+        self._queries: Optional[List[Query]] = None
 
-    def _map_to_query_type(self):
-        """This method maps the query to an API method"""
+    def _map_to_path_query(self):
+        """This method maps the rest_query to API methods"""
         # If not open, run shortest_simple_paths and other queries
         if not self.open:
-            self.api_method = shortest_simple_paths.__name__
+            self._query_list = [(shortest_simple_paths.__name__,
+                                 ShortestSimplePathsQuery(self.rest_query))]
+            self._query_list.extend(self._aux_queries())
         # If open: check if weighted options
         else:
             if _is_weighted(weight=self.weighted,
                             mesh_ids=self.mesh,
                             strict_mesh_id_filtering=self.strict_mesh):
-                self.api_method = open_dijkstra_search.__name__
+                self._query_list = [open_dijkstra_search.__name__,
+                                    DijkstraQuery(self.rest_query)]
             else:
-                self.api_method = bfs_search.__name__
+                self._query_list = [bfs_search.__name__,
+                                    BreadthFirstSearchQuery(self.rest_query)]
 
-    def get_query_type(self) -> str:
-        """Get the query type for the"""
-        if not self.api_method:
-            self._map_to_query_type()
-        return self.api_method
+    def _aux_queries(self) -> \
+            List[Tuple[str, Union[SharedRegulatorsQuery, OntologyQuery]]]:
+        """Get shared interactors and ontological query"""
+        aux_queries = \
+            [('shared_regulators', SharedRegulatorsQuery(self.rest_query)),
+             (shared_parents.__name__, OntologyQuery(self.rest_query))]
+        if self.rest_query.shared_regulators:
+            aux_queries.append(
+                ('shared_targets', SharedTargetsQuery(self.rest_query))
+            )
 
-    def get_query(self) -> Query:
-        """Returns the Query type associated with the search type
+        return aux_queries
+
+    def get_query_list(self) -> List[Tuple[str, Query]]:
+        """Returns a list of (query name, Query) tuples
 
         Returns
         -------
-        Query
-
-        Raises
-        ------
-        ValueError
-            Raised for unknown query types
+        List[Tuple[str, Query]]
         """
-        if not self._query:
-            query_type = self.get_query_type()
-            if query_type == bfs_search.__name__:
-                query = BreadthFirstSearchQuery(self.rest_query)
-            elif query_type == open_dijkstra_search.__name__:
-                query = DijkstraQuery(self.rest_query)
-            elif query_type == shortest_simple_paths.__name__:
-                query = ShortestSimplePathsQuery(self.rest_query)
-            else:
-                raise ValueError(f'Unknown query type {query_type}')
-            self._query = query
-
-        return self._query
-
-    def get_options(self, graph: nx.DiGraph) -> Dict:
-        """Get the run options matching the query"""
-        asq: Query = self.get_query()
-        return asq.run_options(graph=graph)
+        if not self._query_list:
+            self._map_to_path_query()
+        return self._query_list
 
 
 def _is_context_weighted(mesh_id_list: bool, strict_filtering: bool):
