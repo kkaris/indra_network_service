e76c04385721fa367800a9a55789e887c641d9dc
kkaris <karis.klas@gmail.com>
2021-03-18 19:24:09 -0400

    Return mapping instead of list of tuples

--- indra_network_search/query_handler.py
+++ indra_network_search/query_handler.py
@@ -2,7 +2,7 @@
 The QueryHandler's job is to act as a middle layer between the network
 search functionalities and the REST API that receives queries.
 """
-from typing import Optional, List, Tuple, Union
+from typing import List, Tuple, Union, Dict
 
 from indra.explanation.pathfinding import shortest_simple_paths, bfs_search, \
     open_dijkstra_search
@@ -31,50 +31,47 @@ class QueryHandler:
         self.weighted: bool = bool(rest_query.weighted)
         self.mesh: bool = bool(rest_query.mesh_ids)
         self.strict_mesh: bool = rest_query.strict_mesh_id_filtering
-        self._query_list: Optional[List[Tuple[str, Query]]] = None
-        self._queries: Optional[List[Query]] = None
+        self._query_dict: Dict[str, Query] = {}
 
-    def _map_to_path_query(self):
+    def _map_query(self):
         """This method maps the rest_query to API methods"""
         # If not open, run shortest_simple_paths and other queries
         if not self.open:
-            self._query_list = [(shortest_simple_paths.__name__,
-                                 ShortestSimplePathsQuery(self.rest_query))]
-            self._query_list.extend(self._aux_queries())
+            self._query_dict = {shortest_simple_paths.__name__:
+                                ShortestSimplePathsQuery(self.rest_query)}
+            self._query_dict.update(self._aux_queries())
         # If open: check if weighted options
         else:
             if _is_weighted(weight=self.weighted,
                             mesh_ids=self.mesh,
                             strict_mesh_id_filtering=self.strict_mesh):
-                self._query_list = [(open_dijkstra_search.__name__,
+                self._query_dict = [(open_dijkstra_search.__name__,
                                      DijkstraQuery(self.rest_query))]
             else:
-                self._query_list = [(bfs_search.__name__,
+                self._query_dict = [(bfs_search.__name__,
                                      BreadthFirstSearchQuery(self.rest_query))]
 
     def _aux_queries(self) -> \
-            List[Tuple[str, Union[SharedRegulatorsQuery, OntologyQuery]]]:
+            Dict[str, Union[SharedRegulatorsQuery, OntologyQuery]]:
         """Get shared interactors and ontological query"""
         aux_queries = \
-            [('shared_regulators', SharedRegulatorsQuery(self.rest_query)),
-             (shared_parents.__name__, OntologyQuery(self.rest_query))]
+            {'shared_regulators': SharedRegulatorsQuery(self.rest_query),
+             shared_parents.__name__: OntologyQuery(self.rest_query)}
         if self.rest_query.shared_regulators:
-            aux_queries.append(
-                ('shared_targets', SharedTargetsQuery(self.rest_query))
-            )
+            aux_queries['shared_targets'] = SharedTargetsQuery(self.rest_query)
 
         return aux_queries
 
-    def get_query_list(self) -> List[Tuple[str, Query]]:
+    def get_query_list(self) -> Dict[str, Query]:
         """Returns a list of (query name, Query) tuples
 
         Returns
         -------
         List[Tuple[str, Query]]
         """
-        if not self._query_list:
-            self._map_to_path_query()
-        return self._query_list
+        if not self._query_dict:
+            self._map_query()
+        return self._query_dict
 
 
 def _is_context_weighted(mesh_id_list: bool, strict_filtering: bool):
