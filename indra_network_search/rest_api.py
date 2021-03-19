e1ba46f6a98ea7cf86145b186526c3d536bf68d0
kkaris <karis.klas@gmail.com>
2021-03-19 14:38:43 -0400

    Add logging, health endpoint

--- indra_network_search/rest_api.py
+++ indra_network_search/rest_api.py
@@ -1,13 +1,38 @@
 """
 The IndraNetworkSearch REST API
 """
+import logging
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from depmap_analysis.util.io_functions import file_opener
 from .data_models import Results, NetworkSearchQuery
 from .search_api import IndraNetworkSearchAPI
 
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
+
+
+class Health(BaseModel):
+    """Health status"""
+    status: str
+
+
+HEALTH = Health(status='booting')
+
+
+@app.get('/health', response_model=Health)
+async def health():
+    """Returns health status
+
+    Returns
+    -------
+    Health
+    """
+    logger.info('Got health check')
+    return HEALTH
 
 
 @app.post('/query', response_model=Results)
@@ -23,6 +48,7 @@ def query(search_query: NetworkSearchQuery):
     -------
     Results
     """
+    logger.info(f'Got NetworkSearchQuery: {search_query.dict()}')
     results = network_search_api.handle_query(rest_query=search_query)
     return results
 
@@ -37,3 +63,5 @@ network_search_api = IndraNetworkSearchAPI(
     unsigned_graph=file_opener(dir_graph_file),
     signed_node_graph=file_opener(sign_node_graph_file)
 )
+
+HEALTH.status = 'available'
