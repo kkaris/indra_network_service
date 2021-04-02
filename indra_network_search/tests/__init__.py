1e90bbaaadde1ce9440b0f58a32647c308ee1886
kkaris <karis.klas@gmail.com>
2021-04-02 14:23:24 -0400

    Set weights for edges to order them

--- indra_network_search/tests/__init__.py
+++ indra_network_search/tests/__init__.py
@@ -1,3 +1,9 @@
+from depmap_analysis.network_functions.net_functions import _weight_from_belief
+
+
+wm = _weight_from_belief
+
+
 nodes = {'BRCA1': {'ns': 'HGNC', 'id': '1100'},
          'BRCA2': {'ns': 'HGNC', 'id': '1101'},
          'CHEK1': {'ns': 'HGNC', 'id': '1925'},
@@ -11,62 +17,72 @@ nodes = {'BRCA1': {'ns': 'HGNC', 'id': '1100'},
          'NCOA': {'ns': 'FPLX', 'id': 'NCOA'}}            # H (unused in edges)
 
 edge_data = {
-        ('BRCA1', 'AR'): {'belief': 1, 'weight': 2, 'statements': [{
+        ('BRCA1', 'AR'): {'belief': 0.999999,
+                          'weight': wm(0.999999),
+                          'statements': [{
             'stmt_hash': 5603789525715921, 'stmt_type': 'Complex',
             'evidence_count': 1, 'belief': 1, 'source_counts': {'sparser': 1},
             'residue': None, 'weight': 2, 'curated': False, 'position': None,
             'english': 'BRCA1 binds AR.'}]
         },
-        ('BRCA1', 'testosterone'): {'belief': 1, 'weight': 2, 'statements': [{
+        ('BRCA1', 'testosterone'): {'belief': 0.99999,
+                                    'weight': wm(0.99999),
+                                    'statements': [{
             'stmt_hash': 5603789525715922, 'stmt_type': 'Complex',
             'evidence_count': 1, 'belief': 1, 'source_counts': {'sparser': 1},
             'residue': None, 'weight': 2, 'curated': False, 'position': None,
             'english': 'BRCA1 binds testosterone.'}]
         },
-        ('BRCA1', 'NR2C2'): {'belief': 1, 'weight': 2, 'statements': [{
+        ('BRCA1', 'NR2C2'): {'belief': 0.9999,
+                             'weight': wm(0.9999),
+                             'statements': [{
             'stmt_hash': 5603789525715923, 'stmt_type': 'Complex',
             'evidence_count': 1, 'belief': 1, 'source_counts': {'sparser': 1},
             'residue': None, 'weight': 2, 'curated': False, 'position': None,
             'english': 'BRCA1 binds NR2C2.'}]
         },
-        ('BRCA1', 'MBD2'): {'belief': 1, 'weight': 2, 'statements': [{
+        ('BRCA1', 'MBD2'): {'belief': 0.999,
+                            'weight': wm(0.999),
+                            'statements': [{
             'stmt_hash': 5603789525715924, 'stmt_type': 'Complex',
             'evidence_count': 1, 'belief': 1, 'source_counts': {'sparser': 1},
             'residue': None, 'weight': 2, 'curated': False, 'position': None,
             'english': 'BRCA1 binds MBD2.'}]
         },
-        ('BRCA1', 'PATZ1'): {'belief': 1, 'weight': 2, 'statements': [{
+        ('BRCA1', 'PATZ1'): {'belief': 0.99,
+                             'weight': wm(0.99),
+                             'statements': [{
             'stmt_hash': 5603789525715924, 'stmt_type': 'Complex',
             'evidence_count': 1, 'belief': 1, 'source_counts': {'sparser': 1},
             'residue': None, 'weight': 2, 'curated': False, 'position': None,
             'english': 'BRCA1 binds PATZ1.'}]
         },
-        ('AR', 'CHEK1'): {'belief': 0.99, 'weight': 4.1e-05, 'statements': [
+        ('AR', 'CHEK1'): {'belief': 0.9, 'weight': wm(0.9), 'statements': [
             {'stmt_hash': 915990, 'stmt_type': 'Phosphorylation',
              'evidence_count': 1, 'belief': 0.99, 'source_counts': {'pc': 1},
              'residue': 'T', 'weight': 0.23572233352106983, 'curated': True,
              'position': '3387', 'english': 'CHEK1 phosphorylates BRCA2.'}]
         },
-        ('testosterone', 'CHEK1'): {'belief': 0.99, 'weight': 4.1e-05,
-                                    'statements': [
+        ('testosterone', 'CHEK1'):
+            {'belief': 0.9, 'weight': wm(0.9), 'statements': [
             {'stmt_hash': 915991, 'stmt_type': 'Phosphorylation',
              'evidence_count': 1, 'belief': 0.99, 'source_counts': {'pc': 1},
              'residue': 'T', 'weight': 0.23572233352106983, 'curated': True,
              'position': '3387', 'english': 'CHEK1 phosphorylates BRCA2.'}]
         },
-        ('NR2C2', 'CHEK1'): {'belief': 0.99, 'weight': 4.1e-05, 'statements': [
+        ('NR2C2', 'CHEK1'): {'belief': 0.9, 'weight': wm(0.9), 'statements': [
             {'stmt_hash': 915992, 'stmt_type': 'Phosphorylation',
              'evidence_count': 1, 'belief': 0.99, 'source_counts': {'pc': 1},
              'residue': 'T', 'weight': 0.23572233352106983, 'curated': True,
              'position': '3387', 'english': 'CHEK1 phosphorylates BRCA2.'}]
         },
-        ('MBD2', 'CHEK1'): {'belief': 1, 'weight': 2, 'statements': [
+        ('MBD2', 'CHEK1'): {'belief': 0.9, 'weight': wm(0.9), 'statements': [
             {'stmt_hash': 560370, 'stmt_type': 'Complex',
              'evidence_count': 1, 'belief': 1, 'source_counts': {'sparser': 1},
              'residue': None, 'weight': 2, 'curated': False, 'position': None,
              'english': 'MBD2 binds CHEK1.'}]
         },
-        ('PATZ1', 'CHEK1'): {'belief': 1, 'weight': 2, 'statements': [
+        ('PATZ1', 'CHEK1'): {'belief': 0.9, 'weight': wm(0.9), 'statements': [
             {'stmt_hash': 560370, 'stmt_type': 'Complex',
              'evidence_count': 1, 'belief': 1, 'source_counts': {'sparser': 1},
              'residue': None, 'weight': 2, 'curated': False, 'position': None,
