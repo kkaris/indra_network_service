9d14fa8cbec80f76301c11a0b393070c0aef4001
kkaris <karis.klas@gmail.com>
2021-04-20 16:18:12 -0400

    Add Node component

---
+++ indra_network_search/frontend/src/components/Result/Node.vue
@@ -0,0 +1,39 @@
+<template>
+  <span>
+    <a v-if="lookup" :href="lookup" :title="title"><b>{{ name }}</b></a>
+    <b v-else :title="title">{{ name }}</b>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    namespace: {
+      type: String,
+      required: true
+    },
+    identifier: {
+      type: String,
+      required: true
+    },
+    sign: {
+      // Currently unused in this context
+      type: Number,
+      default: null
+    },
+    lookup: {
+      type: String,
+      default: ''
+    },
+  },
+  computed: {
+    title() {
+      return `${this.namespace}:${this.identifier}`
+    }
+  }
+}
+</script>
