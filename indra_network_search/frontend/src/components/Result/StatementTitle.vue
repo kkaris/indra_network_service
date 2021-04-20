d266d271799f3981fc790655f081cd1778878b17
kkaris <karis.klas@gmail.com>
2021-04-20 17:13:00 -0400

    Add component for statement title

---
+++ indra_network_search/frontend/src/components/Result/StatementTitle.vue
@@ -0,0 +1,34 @@
+<template>
+  <span>
+    <Node v-bind="subj_node"/> {{ sentence }} <Node v-bind="obj_node" />
+  </span>
+</template>
+
+<script>
+import Node from "@/components/Result/Node";
+
+export default {
+  components: {Node},
+  props: {
+    subj_node: {
+      type: Object,
+      required: true
+    },
+    obj_node: {
+      type: Object,
+      required: true
+    },
+    sentence: {
+      type: String,
+      required: true,
+    }
+  },
+  computed: {
+    linkedSentence() {
+      return this.sentence
+          .replace(this.subj_node.name, '')
+          .replace(this.obj_node.name, '')
+    }
+  }
+};
+</script>
