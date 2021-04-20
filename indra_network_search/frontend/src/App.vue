ef778a25c187badfde4d0e5bfd9bcc8cae45b86d
kkaris <karis.klas@gmail.com>
2021-04-20 10:53:46 -0400

    Move NetworkSearchForm.vue

--- indra_network_search/frontend/src/App.vue
+++ indra_network_search/frontend/src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import NetworkSearchForm from "@/components/NetworkSearchForm";
+import NetworkSearchForm from "@/views/NetworkSearchForm";
 
 export default {
   name: "App",
