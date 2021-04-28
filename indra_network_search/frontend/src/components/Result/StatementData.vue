6df76086fa3f74305807a38a3ec42304293da634
kkaris <karis.klas@gmail.com>
2021-04-28 14:55:04 -0400

    Add first iteration of StatementData.vue

---
+++ indra_network_search/frontend/src/components/Result/StatementData.vue
@@ -0,0 +1,90 @@
+<template>
+  <h3>
+    <StatementTitle
+      :subj_node="subj_node"
+      :obj_node="obj_node"
+      :sentence="english"
+    /><br/>
+    <!-- Add meta data section -->
+    <p class="stmt-meta">
+      <span>Evidence: {{ evidence_count }} | </span>
+      <span>Curated: {{ curated }} | </span>
+      <span><a :href="db_url_hash">View in INDRA DB</a></span>
+    </p>
+  </h3>
+
+</template>
+
+<script>
+import StatementTitle from "@/components/Result/StatementTitle";
+
+export default {
+  components: {StatementTitle},
+  props: {
+    // Pass nodes to StatmentTitle
+    subj_node: {
+      type: Object,
+      required: true,
+    },
+    obj_node: {
+      type: Object,
+      required: true,
+    },
+    // Match the fields of Class StmtData in indra_network_search/data_models.py
+    stmt_type: {
+      type: String,
+      required: true,
+    },
+    evidence_count: {
+      type: Number,
+      required: true,
+    },
+    stmt_hash: {
+      type: String,
+      required: true,
+    },
+    source_counts: {
+      type: Object,
+      required: true,
+    },
+    belief: {
+      type: Number,
+      required: true,
+    },
+    curated: {
+      type: Boolean,
+      required: true,
+    },
+    english: {
+      type: String,
+      required: true,
+    },
+    weight: {
+      type: [String, Number],
+      default: '',
+    },
+    residue: {
+      type: String,
+      default: '',
+    },
+    position: {
+      type: String,
+      default: '',
+    },
+    initial_sign: {
+      type: Number,
+      default: null,
+    },
+    db_url_hash: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.stmt-meta {
+  background-color: lightcyan;
+}
+</style>
