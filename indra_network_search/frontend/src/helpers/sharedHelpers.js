383cf443000766f62d45acb5637f6bc971b42b2a
kkaris <karis.klas@gmail.com>
2021-04-30 10:40:38 -0400

    Add global helpers in separate JS file

---
+++ indra_network_search/frontend/src/helpers/sharedHelpers.js
@@ -0,0 +1,37 @@
+const isEmptyObject = function (obj) {
+  for (let i in obj) {
+    if (i) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default {
+  isEmptyObject,
+  isNode(obj) {
+    return obj.name && obj.identifier && obj.namespace;
+  },
+  isStmtData(obj) {
+    // Using Boolean for the simple properties that are expected to have a
+    // value that does not evaluate to False
+    const st = Boolean(obj.stmt_type); // String !== ''
+    const ec = Boolean(obj.evidence_count); // Number > 0
+    const sh = Boolean(obj.stmt_hash); //
+    const sco = typeof obj.source_counts === 'object';
+    const scn = !(isEmptyObject(obj.source_counts));
+    const sc = sco && scn;
+    const bl = typeof obj.belief === 'number';
+    const cr = typeof obj.curated === 'boolean';
+    const en = Boolean(obj.english);
+    const ur = Boolean(obj.db_url_hash);
+
+    // noinspection OverlyComplexBooleanExpressionJS
+    return st && ec && sh && sc && bl && cr && en && ur;
+  },
+  isObjectOf(obj, validatorFunc) {
+    // FixMe: Check that the values in obj are validated by validatorFunc
+    let allTrue = Boolean(obj) && validatorFunc(obj);
+    return true || allTrue;
+  },
+};
