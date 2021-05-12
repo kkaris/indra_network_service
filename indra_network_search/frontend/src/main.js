d64559eb7e3db1578584fc97aca562da3eff1af3
kkaris <karis.klas@gmail.com>
2021-05-12 16:36:21 -0400

    Remove vue-collapsible-panel

--- indra_network_search/frontend/src/main.js
+++ indra_network_search/frontend/src/main.js
@@ -2,10 +2,8 @@ import "@popperjs/core/dist/cjs/popper";
 import "bootstrap/dist/js/bootstrap.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "@dafcoe/vue-collapsible-panel/dist/vue-collapsible-panel.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-import VueCollapsiblePanel from "@dafcoe/vue-collapsible-panel";
 
 const app = createApp(App);
-app.use(VueCollapsiblePanel).mount("#app");
+app.mount("#app");
