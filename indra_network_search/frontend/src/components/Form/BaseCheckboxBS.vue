23444df8b6e2f35898cdd05a59927d03e0c204e6
kkaris <karis.klas@gmail.com>
2021-05-13 17:27:01 -0400

    Update labelling for checkboxes

--- indra_network_search/frontend/src/components/Form/BaseCheckboxBS.vue
+++ indra_network_search/frontend/src/components/Form/BaseCheckboxBS.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="form-check">
     <input
-      v-bind="$attrs"
-      type="checkbox"
-      :checked="modelValue"
-      @change="$emit('update:modelValue', $event.target.checked)"
-      class="field form-check-input"
+        class="form-check-input"
+        type="checkbox"
+        v-bind="$attrs"
+        :checked="modelValue"
+        @change="$emit('update:modelValue', $event.target.checked)"
+        :id="strUUID"
     />
     <label
-      v-if="label"
-      class="form-check-label"
+        v-if="label"
+        class="form-check-label"
+        :for="strUUID"
     >{{ label }}</label>
   </div>
 </template>
 
 <script>
+import UniqueID from "@/helpers/BasicHelpers";
+
 export default {
   props: {
     label: {
@@ -24,6 +28,17 @@ export default {
     modelValue: {
       type: Boolean,
       default: false
+    }
+  },
+  setup() {
+    const uuid = UniqueID().getID();
+    return {
+      uuid
+    }
+  },
+  computed: {
+    strUUID() {
+      return `checkbox${this.uuid}`
     }
   }
 }
