b020e9b37d0df642ad5555502dc475eacba349ec
kkaris <karis.klas@gmail.com>
2021-05-24 09:20:12 -0400

    Properly import local stylesheet

--- indra_network_search/frontend/src/main.js
+++ indra_network_search/frontend/src/main.js
@@ -2,7 +2,7 @@ import "@popperjs/core/dist/cjs/popper";
 import "bootstrap/dist/js/bootstrap.min";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "assets/sources.css";
+import "./assets/sources.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
