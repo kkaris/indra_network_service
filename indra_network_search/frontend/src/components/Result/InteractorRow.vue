c0bbbb86327d961536de06912e46b46da507607c
kkaris <karis.klas@gmail.com>
2021-06-22 10:28:18 -0400

    Add component InteractorRow

---
+++ indra_network_search/frontend/src/components/Result/InteractorRow.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- Edge with source -->
+  <div class="row">
+    <Edge v-bind="sourceEdgeData" />
+  </div>
+  <!-- Edge with source -->
+  <div class="row">
+    <Edge v-bind="targetEdgeData" />
+  </div>
+</template>
+
+<script>
+import sharedHelpers from "@/helpers/sharedHelpers";
+import Edge from "@/components/Result/Edge";
+
+export default {
+  components: { Edge },
+  props: {
+    sourceEdgeData: {
+      type: Object,
+      required: true,
+      validator: obj => {
+        return sharedHelpers.isEdgeData(obj)
+      }
+    },
+    targetEdgeData: {
+      type: Object,
+      required: true,
+      validator: obj => {
+        return sharedHelpers.isEdgeData(obj)
+      }
+    }
+  }
+}
+</script>
